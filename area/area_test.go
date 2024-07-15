08ce35e676cc59d3a8c89dca20fd0ea1100c59fc
wooobo <irooori89@gmail.com>
2024-07-15 21:00:56 +0900

    commit

--- area/area_test.go
+++ area/area_test.go
@@ -1,13 +1,13 @@
 package area_test
 
-//import (
-//	"fmt"
-//	"github.com/wooobo/godoc-example/area"
-//)
-//
-//func ExampleSquare_Area() {
-//	s := area.Square{Side: 10}
-//	fmt.Println(s.Area())
-//	// Output:
-//	// 100
-//}
+import (
+	"fmt"
+	"github.com/wooobo/godoc-example/area"
+)
+
+func ExampleSquare_Area() {
+	s := area.Square{Side: 10}
+	fmt.Println(s.Area())
+	// Output:
+	// 100
+}
